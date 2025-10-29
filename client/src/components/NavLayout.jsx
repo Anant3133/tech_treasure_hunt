abad392f8bf8e86c691dd48069a126263e287025
Anant Mathur <anantmg10@gmail.com>
2025-10-30 00:08:34 +0530

    navbar update mobile

--- client/src/components/NavLayout.jsx
+++ client/src/components/NavLayout.jsx
@@ -57,7 +57,7 @@ export default function NavLayout({ children }) {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             type="button" 
-            className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+            className="hidden md:block text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
               Logout
             </motion.button>
             {/* Mobile hamburger */}
